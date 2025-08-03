299eacf22723b5a4418528eea5d6a56cbfee8245
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-08-03 13:38:31 +0800

    class concept

--- dart_basics/lib/main.dart
+++ dart_basics/lib/main.dart
@@ -4,16 +4,61 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar:  AppBar(
+        appBar: AppBar(
           title: Text('Test App'),
+          leading: Icon(IconData(0xe3b2, fontFamily: 'MaterialIcons')),
         ),
-        body: Center(
-          child: Text(
-            'TEST adasdsad 1',
-            style: TextStyle(color: Color.fromARGB(255, 133, 31, 31)),
-          ),
-        ),
+        body: CustomContainer(),
       ),
     ),
   );
+}
+
+class CustomContainer extends StatelessWidget
+{
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context)  
+    => Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(94, 144, 194, 1),
+                Color.fromRGBO(94, 144, 194, 0.5),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://avatars.githubusercontent.com/u/50490077?v=4', 
+                  width: 150, 
+                  height: 150,
+                  fit: BoxFit.contain,
+                  loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                            : null,
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                    );
+                  },
+                  errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                    return Text('Could not load image'); 
+                  },
+                ),
+                SizedBox(height: 20),
+                Text('jjosh102!'),
+              ],
+            ),
+          ),
+        );
 }
